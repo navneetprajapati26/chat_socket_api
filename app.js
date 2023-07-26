d3840c16f9d3e132b39b88512757ee9c7bae1529
Navneet Prajapati <navneetprajapati26@gmail.com>
2023-07-26 20:16:21 +0530

    now port have env

--- app.js
+++ app.js
@@ -1,31 +1,32 @@
-
-const express = require('express');
+import express from "express";
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
+import { createServer } from "http";
+const server = createServer(app);
+import { Server } from "socket.io";
 const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world</h1>");
 });
 
 // Socket.io connection handler
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
   });
-  
-  socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
-    
+
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+
     // Broadcast message to all clients
-    io.emit('chat message', msg); 
+    io.emit("chat message", msg);
   });
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+  console.log(`listening on ${port}`);
 });
